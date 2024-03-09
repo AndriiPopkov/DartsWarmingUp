edb380f9e1c848c7c60305e9acde9aaabdc1e848
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2024-03-09 01:00:11 +0100

    Delete src directory

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware} from 'redux'
-import {thunk} from 'redux-thunk'
-import { counterReducer } from './counterReducer'
-
-const rootReducer = combineReducers({
-    count: counterReducer
-})
-
-export const store = createStore(rootReducer, applyMiddleware(thunk))
